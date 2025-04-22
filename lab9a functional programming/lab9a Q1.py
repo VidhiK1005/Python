67447cca03ad6cdddcdc7d6ea19a10390ef75d97
VidhiK1005 <vidhikewlani20@gmail.com>
2025-04-23 02:57:54 +0530

    Add files via upload

---
+++ lab9a functional programming/lab9a Q1.py
@@ -0,0 +1,15 @@
+def fun():
+    print('i had fun')
+
+def disp():
+    print('i am vidhi')
+
+def msg():
+    print('hoe are you?')
+
+a=fun
+b=disp
+c=msg
+l=[a,b,c]
+for i in l:
+    i()
---
+++ lab9a functional programming/lab9a Q5.py
@@ -0,0 +1,3 @@
+l=['arundhati','varnamala','vidhi','aanchal']
+m=list(filter((lambda x:len(x)<8),l))
+print(m)
