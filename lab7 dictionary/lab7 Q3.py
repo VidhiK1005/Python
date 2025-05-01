605155f10701a1a349e1286464f2425062d119ed
VidhiK1005 <vidhikewlani20@gmail.com>
2025-05-01 15:47:20 +0530

    Update lab7 Q3.py

--- lab7 dictionary/lab7 Q3.py
+++ lab7 dictionary/lab7 Q3.py
@@ -1,5 +1,21 @@
-d={1:{'dept no':12,'salary':12000},4:{'dept no':3,'salary':15000},6:{'dept no':9,'salary':10000},3:{'dept no':12,'salary':15000},8:{'dept no':9,'salary':12000},7:{'dept no':3,'salary':18000},5:{'dept no':3,'salary':19000}}
-ds={}
+d = {
+    1: {'dept no': 12, 'salary': 12000},
+    4: {'dept no': 3, 'salary': 15000},
+    6: {'dept no': 9, 'salary': 10000},
+    3: {'dept no': 12, 'salary': 15000},
+    8: {'dept no': 9, 'salary': 12000},
+    7: {'dept no': 3, 'salary': 18000},
+    5: {'dept no': 3, 'salary': 19000}
+}
+
+sal={}
 for i in d.values():
-    for j in d[i]:
-#pata nai bahot vague question hai isliye choda hai
+    if i['dept no'] not in sal:
+        sal[i['dept no']]=[]
+    sal[i['dept no']].append(i['salary'])
+
+r={}
+for k,v in sal.items():
+    r[k]={'min salary':min(v),'max salary':max(v)}
+
+print(r)
